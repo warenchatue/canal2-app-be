43d43e6e7a695d458e303afc28b99bb8ae7dee14
jordan2204 <anafackjordan@gmail.com>
2024-05-09 22:44:47 +0100

    feat: added db:backup

--- src/features/database/database.cron.ts
+++ src/features/database/database.cron.ts
@@ -9,23 +9,16 @@ export class DatabaseCron {
 
   constructor(private readonly databaseService: DatabaseService) {}
 
-  @Cron('*/5 * * * *') // Every 5 minutes
-  async exportDB0() {
-    this.logger.warn('🚀 Start exporting database...');
-    await this.databaseService.exportCollections(DB_NAME, [], DB_BACKUP_PATH);
-    this.logger.warn('✅ Database exported.');
-  }
+  // @Cron('*/5 * * * *') // Every 5 minutes
+  // async exportDB0() {
+  //   this.logger.warn('🚀 Start exporting database...');
+  //   await this.databaseService.exportCollections(DB_NAME, [], DB_BACKUP_PATH);
+  //   this.logger.warn('✅ Database exported.');
+  // }
 
-  @Cron('0 13 * * *') // Every day at 1 PM
-  async exportDB1() {
-    this.logger.warn('🚀 1- Start exporting database...');
-    await this.databaseService.exportCollections(DB_NAME, [], DB_BACKUP_PATH);
-    this.logger.warn('✅ Database exported.');
-  }
-
-  @Cron('0 23 * * *') // Every day at 11 PM
+  @Cron('0 13,23 * * *') // Every day at 1 PM and 11 PM
   async exportDB2() {
-    this.logger.warn('🚀 2- Start exporting database...');
+    this.logger.warn('🚀 Start exporting database...');
     await this.databaseService.exportCollections(DB_NAME, [], DB_BACKUP_PATH);
     this.logger.warn('✅ Database exported.');
   }
